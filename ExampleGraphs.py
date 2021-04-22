6063773d52dec6fd37acf310c7ecf087bc247036
abhiroopks <abhiroopks@gmail.com>
2021-04-22 15:56:27 -0500

    Added example Takagaki Maze

--- ExampleGraphs.py
+++ ExampleGraphs.py
@@ -4,17 +4,59 @@ import numpy as np
 # T. Nakagaki, H. Yamada, and A. Toth. Maze-solving by an amoeboid organism. Nature, 407:470, 2000.
 def TakagakiMaze():
     
-    N = 23
+    N = 19
     
     # Length, width of edges
     L = np.ones((N,N))
     D = np.zeros((N,N))    
+     
+    # dict of x,y coords for each node. Useful for visualization in networkx
+    pos = {}
+    pos[0] = [-1, -1]
+    pos[1] = [-1, 0]
+    pos[2] = [-1, 1]
+    pos[3] = [-0.67, -1]
+    pos[4] = [-0.67, 0]
+    pos[5] = [-0.67, 0.5]
+    pos[6] = [-0.33, -1]
+    pos[7] = [-0.33, 0]
+    pos[8] = [-0.33, 0.5]
+    pos[9] = [0, 1]
+    pos[10] = [0.33, 1]
+    pos[11] = [0.33, 0.5]
+    pos[12] = [0.33, -0.5]
+    pos[13] = [0.67, 1]
+    pos[14] = [1, -0.5]
+    pos[15] = [1, -1]
+    pos[16] = [0.67, -0.5]
+    pos[17] = [0.67, -1]
+    pos[18] = [0.33, -1]
+    
+    edgesetter = EdgeSetter(D,L,pos)
     
     # initialize the edges and lengths
-    setEdge(D,L,
-   
+    edgesetter.setEdge(D,L,0,1,pos)            
+    edgesetter.setEdge(1,2)            
+    edgesetter.setEdge(3,4)            
+    edgesetter.setEdge(1,4)            
+    edgesetter.setEdge(4,5)            
+    edgesetter.setEdge(6,7)            
+    edgesetter.setEdge(7,8)            
+    edgesetter.setEdge(5,9)            
+    edgesetter.setEdge(2,9)            
+    edgesetter.setEdge(9,10)            
+    edgesetter.setEdge(8,11)            
+    edgesetter.setEdge(10,11)            
+    edgesetter.setEdge(10,13)            
+    edgesetter.setEdge(13,14)            
+    edgesetter.setEdge(14,15)            
+    edgesetter.setEdge(14,16)            
+    edgesetter.setEdge(16,17)            
+    edgesetter.setEdge(12,16)            
+    edgesetter.setEdge(11,12)            
+    edgesetter.setEdge(17,18)            
     
-    
+    return {'L': L, 'D': D, 'pos': pos}
     
 # Returns another example maze
 def exampleMaze0():
@@ -24,19 +66,6 @@ def exampleMaze0():
     # Length, width of edges
     L = np.ones((N,N))
     D = np.zeros((N,N))
-    
-    # initialize the edges and lengths
-    setEdge(D,L,0,1,1)
-    setEdge(D,L,1,9,1)
-    setEdge(D,L,2,9,0.5)
-    setEdge(D,L,1,3,0.5)
-    setEdge(D,L,3,4,1)
-    setEdge(D,L,2,5,1)
-    setEdge(D,L,5,6,0.5)
-    setEdge(D,L,5,8,2)
-    setEdge(D,L,6,7,2)
-    setEdge(D,L,8,7,0.5)
-    
     
     # dict of x,y coords for each node. Useful for visualization in networkx
     pos = {}
@@ -51,6 +80,20 @@ def exampleMaze0():
     pos[8] = [0.5, -1]
     pos[9] = [-1, 1]
     
+    edgesetter = EdgeSetter(D,L,pos)
+    
+    # initialize the edges and lengths
+    edgesetter.setEdge(0,1)
+    edgesetter.setEdge(1,9)
+    edgesetter.setEdge(2,9)
+    edgesetter.setEdge(1,3)
+    edgesetter.setEdge(3,4)
+    edgesetter.setEdge(2,5)
+    edgesetter.setEdge(5,6)
+    edgesetter.setEdge(5,8)
+    edgesetter.setEdge(6,7)
+    edgesetter.setEdge(7,8)
+    
     return {'L': L, 'D': D, 'pos': pos}
     
 """
@@ -58,8 +101,19 @@ Create both edges:
 * frm -> to
 * to -> from
 
+Calculates edge length from node positions
+
 """
-def setEdge(D, L, frm, to, length):
+class EdgeSetter:
     
-    D[frm][to] = D[to][frm] = 1
-    L[frm][to] = L[to][frm] = length
+    def __init__(self, D, L, pos):
+        self.D = D
+        self.L = L
+        self.pos = pos
+
+
+def setEdge(frm, to):
+    
+    length = np.linalg.norm((np.array(self.pos[frm]) - np.array(self.pos[to])), 2)
+    self.D[frm][to] = self.D[to][frm] = 1
+    self.L[frm][to] = self.L[to][frm] = length
